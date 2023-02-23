25d6938ff03b2c4eca17ed9f437f08c6d8151e0e
Christian Rodriguez <christ.rod@gmail.com>
2023-02-23 08:31:24 -0500

    Instalacion Handlebars

--- src/server.js
+++ src/server.js
@@ -1,36 +1,61 @@
-import express from 'express';
-const app = express();
-import routerProducts from "./routes/productos.routes.js";
-import routerCart from "./routes/carrito.routes.js";
-import { fileURLToPath } from 'url'
-import { dirname } from 'path'
+import express from 'express'
+const app = express()
+import routerProducts from "./routes/productos.routes.js"
+import routerCart from "./routes/carrito.routes.js"
+import {__dirname} from './path.js'
 import multer from 'multer'
+import {engine} from 'express-handlebars'
+import * as path from 'path'
 
-const upload = multer ({dest :'src/public/img'})
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+//const upload = multer({dest:'src/public/img'}) Forma basica de utilizar multer
+const storage = multer.diskStorage({
+    destination: (req,file, cb) => {
+      cb(null, 'src/public/img')
+    },
+    filename: (req,file,cb) => {
+      cb(null, `${file.originalname}`)
+    }
+  })
 
-
-import { engine } from 'express-handlebars';
-import * as path from 'path';
+  const upload = multer({storage:storage})
 
 // import {create} from  'express-handlebars'; Servers mas complejos
-
 /*--------------------------------Middlewares---------------------------------------------*/
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use('/static',express.static(__dirname + '/public'))
-
-
-app.engine("handlebars",engine()) //Handlebars
-app.set("view engine", "handlebars") //Handlebars
-app.set("views", path.resolve()) //Handlebars
-
+app.engine("handlebars",engine()) //Config de hbs
+app.set("view engine", "handlebars") // Se definen las vistas
+app.set("views", path.resolve(__dirname, "./views"))
+console.log(__dirname)     //`${__dirname}/views`   .. donde estaran alojadas las vistas
 
 /*---------------------Middlewares Rutas--------------------------------------------------*/
 
 app.use('/api/productos', routerProducts);
 app.use('/api/carrito', routerCart);
+app.use('/static',express.static(__dirname + '/public')) 
+
+app.get('/static',(req,res) =>{
+
+    const user = {
+        nombre: "Christian ",
+        email:"christ.rod@gmail.com",
+        rol:"Manager"
+    }
+
+    const productos= [
+        {id: 1, descripcion:"Producto A", precio: 200, stock :25 },
+        {id: 2, descripcion:"Producto B", precio: 300, stock :30},
+        {id: 3, descripcion:"Producto C", precio: 300, stock :35},
+        {id: 4, descripcion:"Producto D", precio: 300, stock :40},
+        {id: 5, descripcion:"Producto E", precio: 300, stock :50},    
+    ]
+
+ res.render("home",{       titulo: "Tienda Sulpayki",       mensaje: "Lista de Productos",
+        isManager: user.rol === "Manager",
+        user,
+        productos
+    })
+})
 
 app.post('/upload',upload.single('product'), (req,res) => {
     console.log(req.body)
